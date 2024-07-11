958fdf101ba9b40c2d43a555c519ae9c27ec52b2
yustinkeram <yustinhendrite@gmail.com>
2024-07-12 01:10:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ water_prediction = ''
 # Tombol prediksi
 if st.button('Test Prediksi Air'):
     water_prediction = model_water_quality_prediction.predict(input_data)
-    if water_prediction[0] == 1:
+     if water_prediction[0] > 0.5:
         water_prediction = 'Air dapat Diminum'
     else:
         water_prediction = 'Air Tidak dapat Diminum'
